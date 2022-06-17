3afa024343c40ff19fefa65a54805110aac1aa4c
micziz <miczicontent@gmail.com>
2022-06-18 00:06:10 +0200

    INTIAL

---
+++ main.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+## Importing the necessary libraries
+from utils.console import print_markdown
+from rich import console
+import time
+
+# Setup the console
+console = console.Console()
+
+# Print welcome message
+print_markdown("""
+# Welcome to pokeinject!
+""")
+console.print("[blue]This is a tool to inject a custom mod into a pokete.\n")
+
+# Rebuild the mods.py file
+def rebuild():
+    # Get confirmation from the user
+    console.print("[bold red] > We are going to rebuild the mods.py file. This will erase all your modifications. Are you sure? [bold green]y/n")
+    answer = input()
+    if answer == "y":
+        # Erase the file and rebuild it
+        rebuilding = f"""
+#!/usr/bin/env python3
+version = "0.1.0"
+name = "pokeinject-mod"
+
+def mod_p_data(p_data):
+"""
+        console.print("[bold red] > Rebuilding mods.py file.")
+        time.sleep(1)
+        with open("pokete/mods/mods.py", "w") as f:
+            f.write(rebuilding)
+    else:
+        console.print("[bold red] > Cancelled.")
+        exit()
+
+def main():
+    console.print("[bold green] > How many pokete do you want to rename?")
+    pokete = int(input())
+    rebuild()
+    for i in range(pokete):
+        console.print("[bold green] > What is the name of the pokete?")
+        pokete_name = input()
+        console.print("[bold green] > What is the new name of the pokete?")
+        new_name = input()
+        rebuilding = f"""
+    p_data.pokes["{pokete_name}"]["name"] = "{new_name}"
+        """
+        with open("pokete/mods/mods.py", "a") as f:
+            f.write(rebuilding)
+    console.print("[bold green] > Done! Now run __init__.py to reload the mod.")
+
+main()
---
+++ readme.md
@@ -0,0 +1,50 @@
+# PokeInjector
+
+Easaly inject pokete into pokete.
+
+## Usage
+
+```
+pyton3 main.py
+```
+
+## Installation
+
+First, you need to install [pokete dependency's](https://github.com/lxgr-linux/pokete/blob/master/README.md).
+
+Then install PokeInjector dependency's.
+
+```
+pip3 install rich
+```
+
+Then, clone PokeInjector.
+
+```
+git clone https://github.com/micziz/PokeInjector.git
+```
+
+Cd into PokeInjector.
+
+```
+cd PokeInjector
+```
+
+And clone pokete.
+
+```
+git clone https://github.com/lxgr-linux/pokete.git   
+```
+
+You shloud now be able to use PokeInjector without any problems.
+
+## License
+
+<PokeInjector a tool for injecting mods into pokete>
+Copyright (C) <2022-present> <micziz and all contributors>
+
+This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with this program. If not, see <https://www.gnu.org/licenses/>.
